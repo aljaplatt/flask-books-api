489629df8791231b52c2c6fb4c38162c0abf1939
Alex <alja.platt@gmail.com>
2022-07-13 18:12:04 +0100

    mongo connection

---
+++ cli-commands.md
@@ -0,0 +1,7 @@
+<!-- RUN MONGODB  -->
+brew services start mongodb-community@5.0
+<!-- STOP MONGODB  -->
+brew services stop mongodb-community@5.0
+
+<!-- LAUNCH VIRTUALENV -->
+pipenv shell
---
+++ main.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+# cluster = 'mongodb+srv://alex:tertul@cluster0.fvanwtt.mongodb.net/eleclib?retryWrites=true&w=majority'
+cluster = 'mongodb://localhost:27017/eleclib'
+client = MongoClient(cluster)
+
+print('databases:', client.list_database_names())
+db = client.eleclib
+# print(db.books)
+print('collections:', db.list_collection_names()) #! [] local collection name not showing
+
+book1 = {'title': 'East of Eden', 'Synopsis': 'Classic American novel', 'author': 'Steinbeck'}
+
+books = db.books
+
+# result = books.insert_one(book1)
+result = books.find_one()
+print(result)
+# {'_id': ObjectId('62cede112919f323e95e968a'), 'title': 'East of Eden', 'Synopsis': 'Classic American novel', 'author': 'Steinbeck'}
